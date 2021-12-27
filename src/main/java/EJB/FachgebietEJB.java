3d72f0b7f82fda1b1c635765c8b357f4858d4ada
SirSimon04 <simi@engelnetz.de>
2021-12-27 01:05:32 +0100

    added fachgebietEJB

---
+++ src/main/java/EJB/FachgebietEJB.java
@@ -0,0 +1,40 @@
+package EJB;
+
+import Entities.Bewerber;
+import Entities.Fachgebiet;
+import java.util.List;
+import javax.ejb.LocalBean;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+
+@Stateless
+@LocalBean
+public class FachgebietEJB{
+
+    @PersistenceContext
+    private EntityManager em;
+
+    public List<Bewerber> getAll(){
+        return em.createNamedQuery(Fachgebiet.class.getSimpleName() + ".findAll").getResultList();
+    }
+
+    public Fachgebiet add(Fachgebiet b){
+        em.persist(b);
+        em.flush();
+        return b;
+    }
+
+    public Fachgebiet getByName(String name){
+        Query query = em.createNamedQuery(Fachgebiet.class.getSimpleName() + ".findByEmail");
+        query.setParameter("name", name);
+        try{
+            Fachgebiet f = (Fachgebiet) query.getSingleResult();
+
+            return f;
+        }catch(javax.persistence.NoResultException e){
+            return null;
+        }
+    }
+}
