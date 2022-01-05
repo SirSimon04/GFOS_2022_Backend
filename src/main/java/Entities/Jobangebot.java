4b716249cb33ccc127b1885b5bb451583385763d
KLukas04 <krinkelukas@gmail.com>
2022-01-05 16:00:58 +0100

    Jobangebot ISCHEF Attribut

--- src/main/java/Entities/Jobangebot.java
+++ src/main/java/Entities/Jobangebot.java
@@ -29,114 +29,114 @@ import javax.xml.bind.annotation.XmlTransient;
 
 /**
  *
- * @author simon
+ * @author krink
  */
 @Entity
-@Table(name="JOBANGEBOT")
+@Table(name = "JOBANGEBOT")
 @XmlRootElement
 @NamedQueries({
-    @NamedQuery(name="Jobangebot.findAll", query="SELECT j FROM Jobangebot j"),
-    @NamedQuery(name="Jobangebot.findByJobangebotid", query="SELECT j FROM Jobangebot j WHERE j.jobangebotid = :jobangebotid"),
-    @NamedQuery(name="Jobangebot.findByTitle", query="SELECT j FROM Jobangebot j WHERE j.title = :title"),
-    @NamedQuery(name="Jobangebot.findByKurzbeschreibung", query="SELECT j FROM Jobangebot j WHERE j.kurzbeschreibung = :kurzbeschreibung"),
-    @NamedQuery(name="Jobangebot.findByLangbeschreibung", query="SELECT j FROM Jobangebot j WHERE j.langbeschreibung = :langbeschreibung"),
-    @NamedQuery(name="Jobangebot.findByMonatsgehalt", query="SELECT j FROM Jobangebot j WHERE j.monatsgehalt = :monatsgehalt"),
-    @NamedQuery(name="Jobangebot.findByJahresgehalt", query="SELECT j FROM Jobangebot j WHERE j.jahresgehalt = :jahresgehalt"),
-    @NamedQuery(name="Jobangebot.findByUrlaubstage", query="SELECT j FROM Jobangebot j WHERE j.urlaubstage = :urlaubstage"),
-    @NamedQuery(name="Jobangebot.findByVorteile", query="SELECT j FROM Jobangebot j WHERE j.vorteile = :vorteile"),
-    @NamedQuery(name="Jobangebot.findByIstremote", query="SELECT j FROM Jobangebot j WHERE j.istremote = :istremote"),
-    @NamedQuery(name="Jobangebot.findByEinstelldatum", query="SELECT j FROM Jobangebot j WHERE j.einstelldatum = :einstelldatum"),
-    @NamedQuery(name="Jobangebot.findByBewerbungsfrist", query="SELECT j FROM Jobangebot j WHERE j.bewerbungsfrist = :bewerbungsfrist"),
-    @NamedQuery(name="Jobangebot.findByStart", query="SELECT j FROM Jobangebot j WHERE j.start = :start"),
-    @NamedQuery(name="Jobangebot.findByIstbefristet", query="SELECT j FROM Jobangebot j WHERE j.istbefristet = :istbefristet"),
-    @NamedQuery(name="Jobangebot.findByEnde", query="SELECT j FROM Jobangebot j WHERE j.ende = :ende")})
-public class Jobangebot implements Serializable, Comparable<Jobangebot>{
+    @NamedQuery(name = "Jobangebot.findAll", query = "SELECT j FROM Jobangebot j"),
+    @NamedQuery(name = "Jobangebot.findByJobangebotid", query = "SELECT j FROM Jobangebot j WHERE j.jobangebotid = :jobangebotid"),
+    @NamedQuery(name = "Jobangebot.findByTitle", query = "SELECT j FROM Jobangebot j WHERE j.title = :title"),
+    @NamedQuery(name = "Jobangebot.findByKurzbeschreibung", query = "SELECT j FROM Jobangebot j WHERE j.kurzbeschreibung = :kurzbeschreibung"),
+    @NamedQuery(name = "Jobangebot.findByLangbeschreibung", query = "SELECT j FROM Jobangebot j WHERE j.langbeschreibung = :langbeschreibung"),
+    @NamedQuery(name = "Jobangebot.findByMonatsgehalt", query = "SELECT j FROM Jobangebot j WHERE j.monatsgehalt = :monatsgehalt"),
+    @NamedQuery(name = "Jobangebot.findByJahresgehalt", query = "SELECT j FROM Jobangebot j WHERE j.jahresgehalt = :jahresgehalt"),
+    @NamedQuery(name = "Jobangebot.findByUrlaubstage", query = "SELECT j FROM Jobangebot j WHERE j.urlaubstage = :urlaubstage"),
+    @NamedQuery(name = "Jobangebot.findByVorteile", query = "SELECT j FROM Jobangebot j WHERE j.vorteile = :vorteile"),
+    @NamedQuery(name = "Jobangebot.findByIstremote", query = "SELECT j FROM Jobangebot j WHERE j.istremote = :istremote"),
+    @NamedQuery(name = "Jobangebot.findByEinstelldatum", query = "SELECT j FROM Jobangebot j WHERE j.einstelldatum = :einstelldatum"),
+    @NamedQuery(name = "Jobangebot.findByBewerbungsfrist", query = "SELECT j FROM Jobangebot j WHERE j.bewerbungsfrist = :bewerbungsfrist"),
+    @NamedQuery(name = "Jobangebot.findByStart", query = "SELECT j FROM Jobangebot j WHERE j.start = :start"),
+    @NamedQuery(name = "Jobangebot.findByIstbefristet", query = "SELECT j FROM Jobangebot j WHERE j.istbefristet = :istbefristet"),
+    @NamedQuery(name = "Jobangebot.findByEnde", query = "SELECT j FROM Jobangebot j WHERE j.ende = :ende")})
+public class Jobangebot implements Serializable, Comparable<Jobangebot> {
 
     private static final long serialVersionUID = 1L;
     @Id
-    @GeneratedValue(strategy=GenerationType.IDENTITY)
-    @Basic(optional=false)
-    @Column(name="JOBANGEBOTID")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "JOBANGEBOTID")
     private Integer jobangebotid;
-    @Basic(optional=false)
+    @Basic(optional = false)
     @NotNull
-    @Size(min=1, max=128)
-    @Column(name="TITLE")
+    @Size(min = 1, max = 128)
+    @Column(name = "TITLE")
     private String title;
-    @Basic(optional=false)
+    @Basic(optional = false)
     @NotNull
-    @Size(min=1, max=256)
-    @Column(name="KURZBESCHREIBUNG")
+    @Size(min = 1, max = 256)
+    @Column(name = "KURZBESCHREIBUNG")
     private String kurzbeschreibung;
-    @Basic(optional=false)
+    @Basic(optional = false)
     @NotNull
-    @Size(min=1, max=8192)
-    @Column(name="LANGBESCHREIBUNG")
+    @Size(min = 1, max = 8192)
+    @Column(name = "LANGBESCHREIBUNG")
     private String langbeschreibung;
-    @Basic(optional=false)
+    @Basic(optional = false)
     @NotNull
-    @Column(name="MONATSGEHALT")
+    @Column(name = "MONATSGEHALT")
     private int monatsgehalt;
-    @Basic(optional=false)
+    @Basic(optional = false)
     @NotNull
-    @Column(name="JAHRESGEHALT")
+    @Column(name = "JAHRESGEHALT")
     private int jahresgehalt;
-    @Basic(optional=false)
+    @Basic(optional = false)
     @NotNull
-    @Column(name="URLAUBSTAGE")
+    @Column(name = "URLAUBSTAGE")
     private int urlaubstage;
-    @Size(max=2048)
-    @Column(name="VORTEILE")
+    @Size(max = 2048)
+    @Column(name = "VORTEILE")
     private String vorteile;
-    @Basic(optional=false)
+    @Basic(optional = false)
     @NotNull
-    @Column(name="ISTREMOTE")
+    @Column(name = "ISTREMOTE")
     private Boolean istremote;
-    @Basic(optional=false)
+    @Basic(optional = false)
     @NotNull
-    @Column(name="EINSTELLDATUM")
+    @Column(name = "EINSTELLDATUM")
     @Temporal(TemporalType.TIMESTAMP)
     private Date einstelldatum;
-    @Basic(optional=false)
+    @Basic(optional = false)
     @NotNull
-    @Column(name="BEWERBUNGSFRIST")
+    @Column(name = "BEWERBUNGSFRIST")
     @Temporal(TemporalType.TIMESTAMP)
     private Date bewerbungsfrist;
-    @Basic(optional=false)
+    @Basic(optional = false)
     @NotNull
-    @Column(name="START")
+    @Column(name = "START")
     @Temporal(TemporalType.TIMESTAMP)
     private Date start;
-    @Basic(optional=false)
+    @Basic(optional = false)
     @NotNull
-    @Column(name="ISTBEFRISTET")
+    @Column(name = "ISTBEFRISTET")
     private Boolean istbefristet;
-    @Column(name="ENDE")
+    @Column(name = "ENDE")
     @Temporal(TemporalType.TIMESTAMP)
     private Date ende;
-    @JoinColumn(name="ADRESSE", referencedColumnName="ADRESSEID")
+    @JoinColumn(name = "ADRESSE", referencedColumnName = "ADRESSEID")
     @ManyToOne
     private Adresse adresse;
-    @JoinColumn(name="BEWERBUNGSTYP", referencedColumnName="BEWERBUNGSTYPID")
+    @JoinColumn(name = "BEWERBUNGSTYP", referencedColumnName = "BEWERBUNGSTYPID")
     @ManyToOne
     private Bewerbungstyp bewerbungstyp;
-    @JoinColumn(name="FACHGEBIET", referencedColumnName="FACHGEBIETID")
+    @JoinColumn(name = "FACHGEBIET", referencedColumnName = "FACHGEBIETID")
     @ManyToOne
     private Fachgebiet fachgebiet;
-    @JoinColumn(name="ANSPRECHPARTNER", referencedColumnName="PERSONALERID")
+    @JoinColumn(name = "ANSPRECHPARTNER", referencedColumnName = "PERSONALERID")
     @ManyToOne
     private Personaler ansprechpartner;
-    @OneToMany(mappedBy="jobangebot")
+    @OneToMany(mappedBy = "jobangebot")
     private List<Bewerbung> bewerbungList;
 
-    public Jobangebot(){
+    public Jobangebot() {
     }
 
-    public Jobangebot(Integer jobangebotid){
+    public Jobangebot(Integer jobangebotid) {
         this.jobangebotid = jobangebotid;
     }
 
-    public Jobangebot(Integer jobangebotid, String title, String kurzbeschreibung, String langbeschreibung, int monatsgehalt, int jahresgehalt, int urlaubstage, Boolean istremote, Date einstelldatum, Date bewerbungsfrist, Date start, Boolean istbefristet){
+    public Jobangebot(Integer jobangebotid, String title, String kurzbeschreibung, String langbeschreibung, int monatsgehalt, int jahresgehalt, int urlaubstage, Boolean istremote, Date einstelldatum, Date bewerbungsfrist, Date start, Boolean istbefristet) {
         this.jobangebotid = jobangebotid;
         this.title = title;
         this.kurzbeschreibung = kurzbeschreibung;
@@ -151,184 +151,184 @@ public class Jobangebot implements Serializable, Comparable<Jobangebot>{
         this.istbefristet = istbefristet;
     }
 
-    public Integer getJobangebotid(){
+    public Integer getJobangebotid() {
         return jobangebotid;
     }
 
-    public void setJobangebotid(Integer jobangebotid){
+    public void setJobangebotid(Integer jobangebotid) {
         this.jobangebotid = jobangebotid;
     }
 
-    public String getTitle(){
+    public String getTitle() {
         return title;
     }
 
-    public void setTitle(String title){
+    public void setTitle(String title) {
         this.title = title;
     }
 
-    public String getKurzbeschreibung(){
+    public String getKurzbeschreibung() {
         return kurzbeschreibung;
     }
 
-    public void setKurzbeschreibung(String kurzbeschreibung){
+    public void setKurzbeschreibung(String kurzbeschreibung) {
         this.kurzbeschreibung = kurzbeschreibung;
     }
 
-    public String getLangbeschreibung(){
+    public String getLangbeschreibung() {
         return langbeschreibung;
     }
 
-    public void setLangbeschreibung(String langbeschreibung){
+    public void setLangbeschreibung(String langbeschreibung) {
         this.langbeschreibung = langbeschreibung;
     }
 
-    public int getMonatsgehalt(){
+    public int getMonatsgehalt() {
         return monatsgehalt;
     }
 
-    public void setMonatsgehalt(int monatsgehalt){
+    public void setMonatsgehalt(int monatsgehalt) {
         this.monatsgehalt = monatsgehalt;
     }
 
-    public int getJahresgehalt(){
+    public int getJahresgehalt() {
         return jahresgehalt;
     }
 
-    public void setJahresgehalt(int jahresgehalt){
+    public void setJahresgehalt(int jahresgehalt) {
         this.jahresgehalt = jahresgehalt;
     }
 
-    public int getUrlaubstage(){
+    public int getUrlaubstage() {
         return urlaubstage;
     }
 
-    public void setUrlaubstage(int urlaubstage){
+    public void setUrlaubstage(int urlaubstage) {
         this.urlaubstage = urlaubstage;
     }
 
-    public String getVorteile(){
+    public String getVorteile() {
         return vorteile;
     }
 
-    public void setVorteile(String vorteile){
+    public void setVorteile(String vorteile) {
         this.vorteile = vorteile;
     }
 
-    public Boolean getIstremote(){
+    public Boolean getIstremote() {
         return istremote;
     }
 
-    public void setIstremote(Boolean istremote){
+    public void setIstremote(Boolean istremote) {
         this.istremote = istremote;
     }
 
-    public Date getEinstelldatum(){
+    public Date getEinstelldatum() {
         return einstelldatum;
     }
 
-    public void setEinstelldatum(Date einstelldatum){
+    public void setEinstelldatum(Date einstelldatum) {
         this.einstelldatum = einstelldatum;
     }
 
-    public Date getBewerbungsfrist(){
+    public Date getBewerbungsfrist() {
         return bewerbungsfrist;
     }
 
-    public void setBewerbungsfrist(Date bewerbungsfrist){
+    public void setBewerbungsfrist(Date bewerbungsfrist) {
         this.bewerbungsfrist = bewerbungsfrist;
     }
 
-    public Date getStart(){
+    public Date getStart() {
         return start;
     }
 
-    public void setStart(Date start){
+    public void setStart(Date start) {
         this.start = start;
     }
 
-    public Boolean getIstbefristet(){
+    public Boolean getIstbefristet() {
         return istbefristet;
     }
 
-    public void setIstbefristet(Boolean istbefristet){
+    public void setIstbefristet(Boolean istbefristet) {
         this.istbefristet = istbefristet;
     }
 
-    public Date getEnde(){
+    public Date getEnde() {
         return ende;
     }
 
-    public void setEnde(Date ende){
+    public void setEnde(Date ende) {
         this.ende = ende;
     }
 
-    public Adresse getAdresse(){
+    public Adresse getAdresse() {
         return adresse;
     }
 
-    public void setAdresse(Adresse adresse){
+    public void setAdresse(Adresse adresse) {
         this.adresse = adresse;
     }
 
-    public Bewerbungstyp getBewerbungstyp(){
+    public Bewerbungstyp getBewerbungstyp() {
         return bewerbungstyp;
     }
 
-    public void setBewerbungstyp(Bewerbungstyp bewerbungstyp){
+    public void setBewerbungstyp(Bewerbungstyp bewerbungstyp) {
         this.bewerbungstyp = bewerbungstyp;
     }
 
-    public Fachgebiet getFachgebiet(){
+    public Fachgebiet getFachgebiet() {
         return fachgebiet;
     }
 
-    public void setFachgebiet(Fachgebiet fachgebiet){
+    public void setFachgebiet(Fachgebiet fachgebiet) {
         this.fachgebiet = fachgebiet;
     }
 
-    public Personaler getAnsprechpartner(){
+    public Personaler getAnsprechpartner() {
         return ansprechpartner;
     }
 
-    public void setAnsprechpartner(Personaler ansprechpartner){
+    public void setAnsprechpartner(Personaler ansprechpartner) {
         this.ansprechpartner = ansprechpartner;
     }
 
     @XmlTransient
-    public List<Bewerbung> getBewerbungList(){
+    public List<Bewerbung> getBewerbungList() {
         return bewerbungList;
     }
 
-    public void setBewerbungList(List<Bewerbung> bewerbungList){
+    public void setBewerbungList(List<Bewerbung> bewerbungList) {
         this.bewerbungList = bewerbungList;
     }
 
     @Override
-    public int hashCode(){
+    public int hashCode() {
         int hash = 0;
         hash += (jobangebotid != null ? jobangebotid.hashCode() : 0);
         return hash;
     }
 
     @Override
-    public boolean equals(Object object){
+    public boolean equals(Object object) {
         // TODO: Warning - this method won't work in the case the id fields are not set
-        if(!(object instanceof Jobangebot)){
+        if (!(object instanceof Jobangebot)) {
             return false;
         }
         Jobangebot other = (Jobangebot) object;
-        if((this.jobangebotid == null && other.jobangebotid != null) || (this.jobangebotid != null && !this.jobangebotid.equals(other.jobangebotid))){
+        if ((this.jobangebotid == null && other.jobangebotid != null) || (this.jobangebotid != null && !this.jobangebotid.equals(other.jobangebotid))) {
             return false;
         }
         return true;
     }
 
     @Override
-    public String toString(){
+    public String toString() {
         return "Entities.Jobangebot[ jobangebotid=" + jobangebotid + " ]";
     }
-
+    
     @Override
     public Jobangebot clone(){
         Jobangebot output = new Jobangebot(this.jobangebotid);
@@ -350,7 +350,7 @@ public class Jobangebot implements Serializable, Comparable<Jobangebot>{
         output.setTitle(title);
         output.setUrlaubstage(urlaubstage);
         return output;
-    }
+}
 
     @Override
     public int compareTo(Jobangebot j){
