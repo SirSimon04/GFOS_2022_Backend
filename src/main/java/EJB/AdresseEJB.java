4b9e2d53d203f0bc750a8f0baa46d49ca995eeb5
SirSimon04 <simi@engelnetz.de>
2022-01-07 02:42:26 +0100

    Add JavaDov for first Ejbs

--- src/main/java/EJB/AdresseEJB.java
+++ src/main/java/EJB/AdresseEJB.java
@@ -1,14 +1,19 @@
 package EJB;
 
 import Entities.Adresse;
-import Entities.Bewerber;
-import java.util.List;
 import javax.ejb.LocalBean;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-import javax.persistence.Query;
 
+/**
+ * <h1>EJB für Adressen</h1>
+ * <p>
+ * Diese Klasse stellt Methoden bezüglich Adressen bereit.
+ * Sie stellt somit eine Schnittstelle zwischen Webservice und Datenbank dar.</p>
+ *
+ * @author Lukas Krinke, Florian Noje, Simon Engel
+ */
 @Stateless
 @LocalBean
 public class AdresseEJB{
@@ -16,12 +21,24 @@ public class AdresseEJB{
     @PersistenceContext
     private EntityManager em;
 
+    /**
+     * Diese Methode fügt eine neue Adresse in die Datenbank ein
+     *
+     * @param a neue Adresse
+     * @return die eingefügte Adresse mit generierter Id
+     */
     public Adresse add(Adresse a){
         em.persist(a);
         em.flush();
         return a;
     }
 
+    /**
+     * Diese Methode gibt eine Adresse anhand ihrer Id zurück
+     *
+     * @param id AdresseId
+     * @return Adresse
+     */
     public Adresse getById(int id){
         return em.find(Adresse.class, id);
     }
--- src/main/java/EJB/BewerberEJB.java
+++ src/main/java/EJB/BewerberEJB.java
@@ -16,6 +16,15 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
 
+/**
+ * <h1>EJB für Bewerbern</h1>
+ * <p>
+ * Diese Klasse stellt Methoden bezüglich Bewerbern bereit.
+ * Sie stellt somit eine Schnittstelle zwischen Webservice und Datenbank dar.</p>
+ *
+ * @author Lukas Krinke, Florian Noje, Simon Engel
+ */
+
 @Stateless
 @LocalBean
 public class BewerberEJB{
@@ -25,24 +34,52 @@ public class BewerberEJB{
 
     private Tokenizer tokenizer = new Tokenizer();
 
+    /**
+     * Diese Methode gibt alle Bewerber zurück.
+     *
+     * @return Liste mit allen Bewerbern
+     */
     public List<Bewerber> getAll(){
         return em.createNamedQuery(Bewerber.class.getSimpleName() + ".findAll").getResultList();
     }
 
+    /**
+     * Diese Methode fügt einen neuen Bewerber in das System ein.
+     *
+     * @param b neuer Bewerber
+     * @return Der Bewerber mit generierter Id
+     */
     public Bewerber add(Bewerber b){
         em.persist(b);
         em.flush();
         return b;
     }
 
+    /**
+     * Diese Methode löscht einen Bewerber aus der Datenbank
+     *
+     * @param b zu löschender Bewerber
+     */
     public void delete(Bewerber b){
         em.remove(b);
     }
 
+    /**
+     * Diese Methode gibt einen Bewerber anhand seiner Id zurück
+     *
+     * @param id BewerberId
+     * @return Der Bewerber
+     */
     public Bewerber getById(int id){
         return em.find(Bewerber.class, id);
     }
 
+    /**
+     * Diese Methode gibt einen Bewerber anhand seiner E-Mailadresse zurück
+     *
+     * @param mail Die E-Mailadresse
+     * @return Der Bewerber
+     */
     public Bewerber getByMail(String mail){
         Query query = em.createNamedQuery(Bewerber.class.getSimpleName() + ".findByEmail");
         query.setParameter("email", mail);
@@ -55,6 +92,12 @@ public class BewerberEJB{
         }
     }
 
+    /**
+     * Diese Methode gibt einen Bewerber anhand seines Tokens wieder
+     *
+     * @param token Das Webtoken
+     * @return Der Bewerber
+     */
     public Bewerber getByToken(String token){
 
         String mail = tokenizer.getMail(token);
